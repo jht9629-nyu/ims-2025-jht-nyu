a2b9fc419e347a51d5387cfd203dac323d1ec453
John Henry Thompson <78864877+jht9629-nyu@users.noreply.github.com>
2025-03-24 15:08:31 -0400

    moire_shader_3

---
+++ wk02/moire_shader_3/README.md
@@ -0,0 +1,17 @@
+# p5.js Shader Example
+
+# --
+
+-[glitch](https://glitch.com/edit/#!/jelly-glacier-motion?path=uniform.frag%3A76%3A4)
+
+# --
+
+This is an example created as part of the p5.js Shaders project.
+
+**For examples and documentation:** <br>
+[p5.js Shaders Guide](https://itp-xstory.github.io/p5js-shaders/#/) <br>
+[p5.js Shaders Glitch Collection](https://glitch.com/@kcconch/p-5-js-shaders)
+
+**Questions / Comments:** <br>
+**Casey Conchinha:** [Github](https://github.com/kcconch) / Socials: [@kcconch](https://twitter.com/kcconch) / Email: kcconch at gmail dot com <br>
+**Louise Lessél:** [Github](https://github.com/louiselessel) / Socials: @louiselessel
--- wk02/moire_shader_3/sketch.js
+++ wk02/moire_shader_3/sketch.js
@@ -1,3 +1,6 @@
+//
+// https://glitch.com/edit/#!/jelly-glacier-motion?path=uniform.frag%3A76%3A4
+
 // in this example we will send a value from our p5 sketch to the shader
 // these values are called "uniform" variables
 // we will use p5's setUniform function to make this happen
@@ -6,7 +9,7 @@
 // a shader variable
 let theShader;
 
-function preload(){
+function preload() {
   // load the shader
   theShader = loadShader('uniform.vert', 'uniform.frag');
 }
@@ -24,16 +27,14 @@ function draw() {
   // here we're using setUniform() to send our uniform values to the shader
   // set uniform is smart enough to figure out what kind of variable we are sending it,
   // so there's no need to cast (unlike processing)
-  theShader.setUniform("u_resolution", [width, height]);
-  theShader.setUniform("u_time", millis() / 1000.0);
-  theShader.setUniform("u_mouse", [mouseX, map(mouseY, 0, height, height, 0)]);
+  theShader.setUniform('u_resolution', [width, height]);
+  theShader.setUniform('u_time', millis() / 1000.0);
+  theShader.setUniform('u_mouse', [mouseX, map(mouseY, 0, height, height, 0)]);
 
   // rect gives us some geometry on the screen
-  rect(0,0,width,height);
-  
-
+  rect(0, 0, width, height);
 }
 
-function windowResized(){
+function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
