5673736d976a0b688c7e34d364fee552f3656258
John Henry Thompson <78864877+jht9629-nyu@users.noreply.github.com>
2025-03-28 20:33:00 -0400

    wip/sliding_window

--- wk02/Unexpected-cut-mask/a_sketch.js
+++ wk02/Unexpected-cut-mask/a_sketch.js
@@ -86,6 +86,10 @@ function render_layer() {
 }
 
 function render_shader() {
+  //
+  let w = layer.width;
+  let h = layer.height;
+
   // shader() sets the active shader with our shader
   shaderLayer.shader(camShader);
 
@@ -93,12 +97,10 @@ function render_shader() {
   camShader.setUniform('tex0', video);
   // console.log(cam)
   // also send the size of 1 texel on the screen
-  camShader.setUniform('texelSize', [2.5 / width, 2.5 / height]);
+  // camShader.setUniform('texelSize', [2.5 / width, 2.5 / height]);
+  camShader.setUniform('texelSize', [2.5 / w, 2.5 / h]);
   // can play around with the ratio for cool effects like edge blurring
   // (original 1.0 for both)
-
-  let w = layer.width;
-  let h = layer.height;
 
   // shaderLayer.ellipse(0, 0, width, height);
   shaderLayer.rect(0, 0, w, h);
@@ -137,10 +139,11 @@ function render_shader() {
 // image(img, x, y, [width], [height])
 // image(img, dx, dy, dWidth, dHeight, sx, sy, [sWidth], [sHeight], [fit], [xAlign], [yAlign])
 
+// --
 function framesPerSecond() {
   return frameRate().toFixed(2);
 }
-// --
+
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
